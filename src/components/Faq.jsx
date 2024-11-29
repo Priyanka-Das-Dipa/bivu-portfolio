dd8c2ad7c32c046e64d00a9e74f5897530f15f87
Priyanka Das Dipa <priyankadipa9102@gmail.com>
2024-11-30 02:57:04 +0600

    working on FAQ

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,32 +1,139 @@
 const Faq = () => {
   return (
     <section className=" min-h-screen max-w-[1200px] mx-auto ">
-      <div className="collapse collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" defaultChecked />
-        <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
+      <div className="flex ">
+
+      <div>
+
+      </div>
+      <div>
+        <div className="flex items-center justify-start">
+          <div className="bg-primary-gradient w-2 h-6 mr-2"></div>
+          <h1 className="text-3xl font-bold">Have any questions?</h1>
         </div>
-        <div className="collapse-content">
-          <p>hello</p>
+        <p className="text-lg font-medium py-2">
+          Feel free to explore this section to find answers to common questions
+          or address any queries you might have about my services.
+        </p>
+        <div className="collapse collapse-arrow bg-base-200">
+          <input type="radio" name="my-accordion-2" defaultChecked />
+          <div className="collapse-title text-xl font-medium">
+            1. What tools do you use?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Google Analytics 4, Google Tag Manager, Google Ads Conversion
+              Tracking, Server Side Tracking, Server - GCP: Google Cloud
+              Platform, Stape, Google Tag Assistant., Datalayer Checker, Tag
+              Assistant Companion etc.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200 mt-2">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            2. Do you provide Lead and Ecommerce Conversion Tracking Setup?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Yes, I can assist you with setting up lead or Ecommerce conversion
+              tracking for all Conversions like Google Analytics 4, Google Ad,
+              Facebook Pixel, Tiktok Pixel etc. If you have particular
+              requirements, Please provide more details about your current
+              setup. I&apos;ll be happy to help further.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200 mt-2">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            3. Which CMS platforms do you work with?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Shopify (with new recommended data layer by Shopify), Wordpress,
+              Woocommerce, Wix, Squarespace, ClickFunnel, PrestaShop,
+              BigCommerce, Magento, Kajabi etc. For a Custom website or other
+              platform contact me.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200 mt-2">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            4. Can I customize my package based on my store&apos;s needs?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Absolutely! Let&apos;s discuss your unique requirements for a
+              tailored solution
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200 mt-2">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            5. How long before I see results?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Immediate improvements can be noticed, with continuous enhancement
+              over tim
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200 mt-2">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            6. Why did you Choose Me?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Delivery on expected. Fast Response. Full Client Satisfaction.
+              Expert-Level Support.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200 mt-2">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            7. Why Do We Need to Setup the Facebook Conversion API?
+          </div>
+          <div className="collapse-content">
+            <p>
+              The Facebook conversions API will help you collect accurate data
+              and improve the performance of your Facebook ads in a post-iOS 14
+              update world.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200 mt-2">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            8. Do you have a guide about Google analytics and Google tag
+            manager?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Yes I&apos;ll guide you properly about Google analytics, Google
+              tag manager and how to check data like traffic source, medium,
+              events, goals and conversions into Google analytics.
+            </p>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-200 mt-2">
+          <input type="radio" name="my-accordion-2" />
+          <div className="collapse-title text-xl font-medium">
+            9. Will You Provide Support After Order Completion?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Definitely, if you need any support after order completion, I am
+              ready to help you.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="collapse collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" />
-        <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
-        </div>
-        <div className="collapse-content">
-          <p>hello</p>
-        </div>
-      </div>
-      <div className="collapse collapse-arrow bg-base-200">
-        <input type="radio" name="my-accordion-2" />
-        <div className="collapse-title text-xl font-medium">
-          Click to open this one and close others
-        </div>
-        <div className="collapse-content">
-          <p>hello</p>
-        </div>
       </div>
     </section>
   );

867c3beaf09b9ae2b3c076f754ba3105343cf33e
Priyanka Das Dipa <priyankadipa9102@gmail.com>
2024-12-09 23:06:43 +0600

    footer is complete

--- src/components/ClientReview.jsx
+++ src/components/ClientReview.jsx
@@ -10,78 +10,6 @@ import img9 from "../../src/assets/img9.jpg";
 import img10 from "../../src/assets/img10.jpg";
 
 const ClientReview = () => {
-  const data = [
-    {
-      name: "Sharma",
-      country: "Netherlands",
-      comment:
-        "I am incredibly grateful for the work Bivuti has done for me. Where even Shopify experts couldn’t find a solution, he solved my issue completely in just one day! Thanks to his expertise, my conversion campaigns are finally working as they should, and I can confidently start running my campaigns. Orchona communicated clearly and understood exactly what was needed. He quickly and professionally made the technical adjustments, including activating the AddToCart event, which hadn’t been working previously due to a custom cart setup. In short, a true expert! I would definitely recommend Bivuti to anyone looking for fast and skilled Shopify assistance.",
-      image: img1,
-    },
-    {
-      name: "Rishadul Islam",
-      country: "Bangladesh",
-      comment:
-        "I have recently hired Vibhuti Bhushan Malik for one of my project that includes setting up of -> JS coding to shopify store, Google tag managers, Advanced pixel setup, Server Config, Conversion API, Google analytics 4, Event match quality. And I am quite pleased with the outcome he was able to bring to the project I definitely consider working with him again on a future projects.",
-      image: img2,
-    },
-    {
-      name: "Max",
-      country: "USA",
-      comment: "Bivuti is professional, fast, and thorough.",
-      image: img3,
-    },
-    {
-      name: "Itamar Jon",
-      country: "Italy",
-      comment:
-        "Thank you for the help, I appreciate the good customer service.",
-      image: img4,
-    },
-    {
-      name: "Adi Avram",
-      country: "Romania",
-      comment:
-        "I am very appreciative of Bivuti's clear and professional communication, strategic insights, and partnership in our Facebook Pixel error. He went above and beyond in making the process comprehensive and moving us closer to our goals. I look forward to working together again!",
-      image: img5,
-    },
-    {
-      name: "Ortal",
-      country: "Israel",
-      comment:
-        "Bivuti is great to work with—very professional, knowledgeable, and communicative. He answered all my questions and completed the work successfully. Thank you very much!",
-      image: img6,
-    },
-    {
-      name: "Mark",
-      country: "USA",
-      comment:
-        "I thought Bivuti Vussion Mallik was excellent! We worked together to get the website working correctly. I’m pleased with the work he did and would like to work with him again.",
-      image: img7,
-    },
-    {
-      name: "David",
-      country: "USA",
-      comment:
-        "Great work, connected my TikTok pixel to Google Tag Manager to monitor conversions. Fast and very professional. Will hire again.",
-      image: img8,
-    },
-    {
-      name: "CT Marketing Solutions",
-      country: "UK",
-      comment:
-        "Excellent Data analytics person connected my TikTok account to my GTM. Bivuti is very professional and technically very good.",
-      image: img9,
-    },
-    {
-      name: "Mollins",
-      country: "Spain",
-      comment:
-        "Bivuti was a great help regarding the Consent Mode V2 implementation & Google Analytics 4. He replied, acted, and delivered his job very fast with proven results. Communication was very good as well. I am very happy with his job. I will contact him every time I need conversion tracking support.",
-      image: img10,
-    },
-  ];
-
   return (
     <section className="max-w-[1200px] mx-auto">
       <div className="py-10">
@@ -93,11 +21,206 @@ const ClientReview = () => {
           From my satisfied clients
         </p>
       </div>
-      <div className="carousel carousel-center rounded-box gap-10">
-       
-      </div>
+      <div className="carousel carousel-center rounded-box">
+        {/* card 1 */}
+        <div className="carousel-item">
+          <div className="card card-side bg-base-100 shadow-xl ">
+            <figure>
+              <img src={img1}  alt="image_loading" className="object-contain" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-bold uppercase">Sharma</h2>
+              <p className="text-justify text-sm leading-relaxed break-words">
+                I am incredibly grateful for the work Bivuti has done for me.
+                Where even Shopify experts couldn’t find a solution, he solved
+                my issue completely <br /> in just one day! Thanks to his
+                expertise, my conversion campaigns are finally working as they
+                should, and I can confidently start running
+                <br /> my campaigns. Orchona communicated clearly and understood
+                exactly what was needed. He quickly and professionally made the
+                technical <br /> adjustments, including activating the AddToCart
+                event, which hadn’t been working previously due to a custom cart
+                setup. In short, a true expert! <br />I would definitely
+                recommend Bivuti to anyone looking for fast and skilled Shopify
+                assistance.
+              </p>
+              <p>
+                - <span className="font-semibold">Netherlands</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* card 2 */}
+        <div className="carousel-item ">
+          <div className="card card-side bg-base-100 shadow-xl ">
+            <figure>
+              <img src={img2} alt="image_loading" className="object-contain" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-bold uppercase">Rishadul Islam</h2>
+              <p className="text-justify text-sm leading-relaxed break-words">
+                I have recently hired Vibhuti Bhushan Malik for one of my
+                project that includes setting up of -> JS coding to shopify
+                store, Google tag <br /> managers, Advanced pixel setup, Server
+                Config, Conversion API, Google analytics 4, Event match quality.
+                And I am quite pleased with the <br /> outcome he was able to
+                bring to the project I definitely consider working with him
+                again on a future projects.
+              </p>
+              <p>
+                - <span className="font-semibold">Bangladesh</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* card 3 */}
+        <div className="carousel-item">
+          <div className="card card-side bg-base-100 shadow-xl ">
+            <figure>
+              <img src={img3} alt="image_loading" className="object-contain" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-bold uppercase">Max</h2>
+              <p className="text-justify text-sm leading-relaxed break-words">
+                Bivuti is professional, fast, and thorough.
+              </p>
+              <p>
+                - <span className="font-semibold">USA</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* card 4 */}
+        <div className="carousel-item">
+          <div className="card card-side bg-base-100 shadow-xl ">
+            <figure>
+              <img src={img4} alt="image_loading" className="object-contain" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-bold uppercase">Itamar Jon</h2>
+              <p className="text-justify text-sm leading-relaxed break-words">
+                Thank you for the help, I appreciate the good customer service.
+              </p>
+              <p>
+                - <span className="font-semibold">Italy</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* card 5 */}
+        <div className="carousel-item">
+          <div className="card card-side bg-base-100 shadow-xl ">
+            <figure>
+              <img src={img5} alt="image_loading" className="object-contain" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-bold uppercase">Adi Avram</h2>
+              <p className="text-justify text-sm leading-relaxed break-words">
+                I am very appreciative of Bivuti's clear and professional
+                communication, strategic insights, and partnership in our <br />{" "}
+                Facebook Pixel error. He went above and beyond in making the
+                process comprehensive and moving us closer to <br /> our goals.
+                I look forward to working together again!
+              </p>
+              <p>
+                - <span className="font-semibold">Romania</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* card 6 */}
+        <div className="carousel-item">
+          <div className="card card-side bg-base-100 shadow-xl ">
+            <figure>
+              <img src={img6} alt="image_loading" className="object-contain" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-bold uppercase">Ortal</h2>
+              <p className="text-justify text-sm leading-relaxed break-words">
+                Bivuti is great to work with—very professional, knowledgeable,
+                and communicative. He answered all my questions and completed
+                the work successfully. <br />
+                Thank you very much!
+              </p>
+              <p>
+                - <span className="font-semibold">Israel</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* card 7 */}
+        <div className="carousel-item">
+          <div className="card card-side bg-base-100 shadow-xl ">
+            <figure>
+              <img src={img7} alt="image_loading" className="object-contain" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-bold uppercase">Mark</h2>
+              <p className="text-justify text-sm leading-relaxed break-words">
+                I thought Bivuti Vussion Mallik was excellent! We worked
+                together to get the website working correctly. I am pleased{" "}
+                <br /> with the work he did and would like to work with him
+                again.
+              </p>
+              <p>
+                - <span className="font-semibold">USA</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* card 8 */}
+        {/* <div className="carousel-item">
+          <div className="card card-side bg-base-100 shadow-xl ">
+            <figure>
+              <img src={img8} alt="image_loading" className="object-contain" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-bold uppercase">David</h2>
+              <p className="text-justify text-sm leading-relaxed break-words">
+              Great work, connected my tiktok pixel to google tag manager to monitor conversions. Fast and very professional. Will hire again.
 
-  
+              </p>
+              <p>
+                - <span className="font-semibold">USA</span>
+              </p>
+            </div>
+          </div>
+        </div> */}
+        {/* card 9 */}
+        {/* <div className="carousel-item">
+          <div className="card card-side bg-base-100 shadow-xl ">
+            <figure>
+              <img src={img9} alt="image_loading" className="object-contain" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-bold uppercase"> CT Marketing Solutions</h2>
+              <p className="text-justify text-sm leading-relaxed break-words">
+              Excellent Data analytics person connected my TikTok account to my GTM. Bivuti is very professional and technically very good.
+              </p>
+              <p>
+                - <span className="font-semibold">UK</span>
+              </p>
+            </div>
+          </div>
+        </div> */}
+        {/* card 10 */}
+        {/* <div className="carousel-item">
+          <div className="card card-side bg-base-100 shadow-xl ">
+            <figure>
+              <img src={img10} alt="image_loading" className="object-contain" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title text-bold uppercase">Mollins</h2>
+              <p className="text-justify text-sm leading-relaxed break-words">
+              Bivuti was a great help regarding the Consent Mode V2 implementation & Google Analytics 4. He replied, <br />act and delivered his job very fast with proven results. Communication was very goood as well. I am very happy <br /> with his job. I will contact him everytime I need conversion tracking support.
+              </p>
+              <p>
+                - <span className="font-semibold">Spain</span>
+              </p>
+            </div>
+          </div>
+        </div> */}
+      </div>
     </section>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,59 @@
+import { AiOutlineDiscord } from "react-icons/ai";
+import { CiFacebook, CiLinkedin } from "react-icons/ci";
+import { SlSocialSkype } from "react-icons/sl";
+
 const Footer = () => {
   return (
-    <div className="text-center bg-[#F5F4FB] text-base-content border-base-300 border-t px-10 py-8">
-      <p>@Copyright. All rights reserved by Bivuti Vussion Mallik.</p>
-    </div>
+    <section className="text-base-content py-10 bg-[#F5F4FB]">
+      <footer className="footer max-w-[1200px] mx-auto ">
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover"> FACEBOOK PIXEL/CONVERSION API</a>
+          <a className="link link-hover"> GOOGLE TAG MANAGER</a>
+          <a className="link link-hover"> GOOGLE ANALYTICS 4/GA4</a>
+          <a className="link link-hover">
+            GOOGLE ADS CONVERSION TRACKING AND REMARKETING TAG
+          </a>
+          <a className="link link-hover">Pinterest Tag & Conversion API</a>
+          <a className="link link-hover">
+            Microsoft Advertising/Bing Ads Tracking
+          </a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Price</a>
+          <a className="link link-hover">Reviews</a>
+        </nav>
+        <nav>
+          <h6 className="footer-title">Social</h6>
+          <div className="grid grid-flow-col gap-4">
+            <a
+              href="https://www.linkedin.com/in/bivuti-vussion-mallik/"
+              target="_blank"
+            >
+              <CiLinkedin className="text-black text-4xl" />
+            </a>
+            <a href="skype:live:.cid.b079b4dabb308aff?chat">
+              <SlSocialSkype className="text-black text-4xl" />
+            </a>
+            <a href="https://www.facebook.com/bivutim" target="_blank">
+              <CiFacebook className="text-black text-4xl" />
+            </a>
+            <a href=" https://discord.com/channels/@ads_bivuti" target="_blank">
+              <AiOutlineDiscord className="text-black text-4xl" />
+            </a>
+            <link rel="stylesheet" href="" />
+          </div>
+        </nav>
+      </footer>
+      <div className="text-center text-base-content border-base-300  px-10 pt-10">
+        <p className="border-t pt-2">
+          @Copyright. All rights reserved by Bivuti Vussion Mallik.
+        </p>
+      </div>
+    </section>
   );
 };
 
